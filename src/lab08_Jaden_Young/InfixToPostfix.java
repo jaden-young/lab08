9b152499db19c0253e159fe0d978df41d85db3ca
jaden <jaden@HAL-9000>
2015-04-16 12:57:32 -0500

    This shit should be good

--- src/lab08_Jaden_Young/InfixToPostfix.java
+++ src/lab08_Jaden_Young/InfixToPostfix.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Contains 1 static method that converts an infix expression into postfix
  * notation.
- * Only recognizes +, -, *, /, and ()
+ * Only recognizes +, -, *, /, x, and ()
  * @author Jaden Young
  */
 public class InfixToPostfix {
@@ -16,63 +16,56 @@ public class InfixToPostfix {
 	 * @param infixExpression Expression to convert
 	 * @return String containing converted expression in postfix notation
 	 */
-	public static String convert(String infixExpression) {
+	public static String convert(String infixExpression) throws 
+			IllegalArgumentException {
 		Scanner scan = new Scanner(infixExpression);
 		String postfix = "";
 		LinkedStack<String> opStack = new LinkedStack<>();
 		
-		//This. Is. Ugly. Please don't look at it. It works ok?
 		while(scan.hasNext()) {
 			String token = scan.next();
 			switch(token) {
-				//always push an opening parenthesis
 				case "(":
 					opStack.push(token);
 					break;
-				//move operators from stack to string until the matching 
-				//parenthesis, then get rid of the opening parenthesis
 				case ")":
-					while(!opStack.top().equals("(")) {
+					while(!opStack.top().equals("("))
 						postfix += opStack.pop() + " ";
-					}
 					opStack.pop();
 					break;
-				//with ops of same priority, move the current op to the string,
-				//then push new op once one of higher priority is reached
-				case "+":
-				case "-":
-					while(!opStack.isEmpty() 
-							&& (opStack.top().equals("+") 
-								|| opStack.top().equals("-"))) {
-						postfix += opStack.pop() + " ";
-					}
-					opStack.push(token);
-					break;
-				//with ops of same priority, move the current op to the string,
-				//then push new op once one of higher priority is reached
-				case "*":
-				case "/":
-				case "x":
-					//putting this many conditions in makes me sick
-					while(!opStack.isEmpty() 
-							&& (opStack.top().equals("*") 
-								|| opStack.top().equals("/")
-								|| opStack.top().equals("x"))) {
-						postfix += opStack.pop() + " ";
-					}
-					opStack.push(token);
-					break;
-				//any number (or anything, since we're not checking for invalid
-				//tokens)
 				default:
-					postfix += token + " ";
+					if(isOperator(token)) 
+						opStack.push(token);
+					else
+						postfix += token + " ";
 			}
 		}
-		//move remaining operators in stack to string
-		while(!opStack.isEmpty()) {
+		while(!opStack.isEmpty())
 			postfix += opStack.pop() + " ";
-		}
-		//It's over. It's finally over.
 		return postfix;
 	}
+	
+	private static boolean isOperator(String input) {
+		return input.equals("+") 
+				|| input.equals("-") 
+				|| input.equals("*")
+				|| input.equals("/")
+				|| input.equals("x");
+	}
+
+//	private static int priority(String operator) {
+//		switch(operator) {
+//			case "(":
+//				return 0;
+//			case "+":
+//			case "-":
+//				return 1;
+//			case "*":
+//			case "/":
+//			case "x":
+//				return 2;
+//			default:
+//				throw new IllegalArgumentException("Invalid operator");
+//		}
+//	}
 }
